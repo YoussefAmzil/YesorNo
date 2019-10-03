23e1e168e4c8eb51a5a555828e3dbbf8793afcc1
YoussefAmzil <youssefamzil98@gmail.com>
2019-10-03 01:26:48 +0100

    first commit

---
+++ src/components/Yes.vue
@@ -0,0 +1,89 @@
+<template>
+<div class="all" >
+  <div class="img">
+  <img src="yes-no-maybe.jpeg" alt="" width="40%" height="10%">
+</div>
+<div class="box">
+  <input type="text"  class="qst" pattern="" placeholder="Ask a Question with Yes or No Answer ?"  v-model="text">
+  <center>
+    <button @click='getAnswer'>Answer me !!</button>
+  </center>
+</div>
+
+</div>
+
+</template>
+
+<script>
+import axios from 'axios';
+import VueSweetalert2 from 'vue-sweetalert2';
+export default {
+  name: 'HelloWorld',
+    data(){ 
+    return{
+      answer:'',
+      text:'',
+    }   
+  },
+  methods:{
+   getAnswer(){
+       axios.get('https://yesno.wtf/api').then(response=>{
+            this.answer= response.data;
+            this.$swal({
+    		  title: '<h1 >'+this.answer.answer.toUpperCase()+'</h1>',
+          // add a custom html tags by defining a html method.
+          html:
+            '<img src='+this.answer.image+'>',
+          showCloseButton: true,
+          showCancelButton: true,
+          focusConfirm: false,
+        })
+          }).catch(error=>{
+              console.log(error);
+          })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.all{
+}
+button{
+  
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 12px 22px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+.box{
+display: inline;
+border:  black 0.2px;
+float: left;
+margin-left: 15%;
+width: 70%;
+padding: 1.2%;
+-moz-box-shadow:    3px 3px 5px 6px #ccc;
+  -webkit-box-shadow: 3px 3px 5px 6px #ccc;
+  box-shadow:         3px 3px 5px 6px #ccc;
+}
+.qst{
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  box-sizing: border-box;
+}
+.img{
+  float: left;
+  margin-left: 40%;
+}
+</style>
